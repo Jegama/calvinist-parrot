7c59d7ca2c7fb9133c2c4f9702bcdbe0de047c6e
Jesus Mancilla <jegama70@gmail.com>
2025-10-31 12:45:04 -0500

    debugging vercel

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,10 @@ const nextConfig = {
       },
     ];
   },
+  outputFileTracingIncludes: {
+    '/api/(.*)': ['./node_modules/.prisma/client/**/*'],
+    '/(.*)': ['./node_modules/.prisma/client/**/*'],
+  },
 }
 
 export default nextConfig
